88c4b36f7119d9c8198577b2c16cd21be64faf4a
mphomosia194 <mphomosia194@gmail.com>
2026-05-28 15:20:05 +0200

    Create basic React dashboard application

---
+++ react_intro/task_0/dashboard/src/App.jsx
@@ -0,0 +1,29 @@
+import holbertonLogo from './assets/holberton-logo.jpg';
+
+function App() {
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <div className="App">
+      <div className="App-header">
+        <img
+          src={holbertonLogo}
+          className="App-logo"
+          alt="holberton logo"
+        />
+
+        <h1>School dashboard</h1>
+      </div>
+
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+      </div>
+
+      <div className="App-footer">
+        <p>Copyright {currentYear} - holberton School</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
