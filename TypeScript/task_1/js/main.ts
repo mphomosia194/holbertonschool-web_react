feeeb02db3c6cda708f7e09006ec284c8190c584
mphomosia194 <mphomosia194@gmail.com>
2026-06-08 07:57:57 +0000

    Typescript tasks

---
+++ TypeScript/task_1/js/main.ts
@@ -0,0 +1,43 @@
+interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [key: string]: any;
+}
+
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (
+  firstName: string,
+  lastName: string
+): string => `${firstName.charAt(0)}. ${lastName}`;
+
+const teacher3: Teacher = {
+  firstName: 'John',
+  fullTimeEmployee: false,
+  lastName: 'Doe',
+  location: 'London',
+  contract: false,
+};
+
+console.log(teacher3);
+
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+
+console.log(director1);
+
+console.log(printTeacher('John', 'Doe'));
---
+++ TypeScript/task_4/js/subjects/Subject.ts
@@ -0,0 +1,13 @@
+/// <reference path="./Teacher.ts" />
+
+namespace Subjects {
+  export class Subject {
+    teacher!: Teacher;
+
+    setTeacher(teacher: Teacher): void {
+      this.teacher = teacher;
+    }
+  }
+}
+
+export = Subjects;
